48abff8916a89565f43432e2fcf48ac61d89fdb2
DESKTOP-NVL0PV4\Alex <alexander.vanhorn@utp.ac.pa>
2021-12-04 04:56:41 -0500

    pagina principal y otras cosas

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <!--CSS Externo para modificaciones-->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- JavaScript Externo -->
+    <script src="style.js"></script>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+    <title>VIII Congreso IESTEC UTP</title>
+
+    <!--Imagen de la barra de título-->
+    <link rel="shortcut icon" href="C:\xampp\htdocs\Prueba proyecto final DS7\images\logo_utp_1_72.png" type="image/x-icon">
+</head>
+
+<body>
+
+    <?php
+    // You can also use comments to leave out parts of a code line
+    $x = 5 + 5;
+    echo $x;
+    ?>
+
+    <img class ="logo-utp" src="logo_utp_web.png">
+
+
+    <h1 class="titulo-1">VII Congreso Internacional de Ingeniería, Ciencias y Tecnología</h1>
+    <h3 class="subtitulo-1">Ciencias, Tecnología, Sostenibilidad e Inovación: Pilares para la recuperación económica</h3>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<footer>
+    <br><br>
+    @2021 Universidad Tecnológica de Panamá || All rights reserved || Powered by UTP
+    <br><br><br>
+</footer>   
+
+</body>
+</html>
