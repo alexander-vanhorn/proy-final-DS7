9ac0867c4cddf5340f7f9580108b45ece2c377ff
Jenia-Castillo <66661964+Jenia-Castillo@users.noreply.github.com>
2021-12-05 22:20:51 -0500

    Areas de interes
    areas de interes y un monton de imagenes

---
+++ secciones/areas_de_interes.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="areas_de_interes.css">
+        <title>Areas de interes</title>
+    </head>
+    <body>
+        <br>
+        <br>
+        <h4 ><center> <b>Áreas de Interés</b></center></h4>
+        <br>
+        <div class="texto_areasinteres"> Las áreas de interés de estas conferencias se listan a continuación pero no estan limitadas a:</div>
+        
+        <br>
+    
+ 
+            <!--imagenes-->
+
+                              
+    
+
+                
+            
+<div class="table-responsive container">
+
+<table  width="50%" border="0" align="center" >
+    
+  
+  <!--Agroindustria 
+Ciencias básicas 
+economía y sociedad 
+educación en ingeniería 
+energía y ambiente 
+gestión empresarial emprendimiento e innovación 
+infraestructura 
+logística y transporte 
+robótica automatización e inteligencia artificial 
+tecnologías de la información y comunicación 
+tecnologías emergentes 
+-->
+  
+    <tbody>
+
+          <tr>
+            <tr> 
+              <td align="center"> <img  width="90" height="90" src="../imagenes/agroindustria.png"></td>
+              <td align="center"> <img width="90" height="90" src="../imagenes/ciencias.png"></td>
+              <td align="center"> <img width="90" height="90" src="../imagenes/economia.png"></td>
+              <td align="center"> <img width="90" height="90" src="../imagenes/educacion.png"> </td>
+              
+            </tr>   
+            <tr>
+            
+              <td align="center"> Agroindustria</td>
+              <td align="center">Ciencias <br> basicas</td>
+              <td align="center">Economia<br>y sociedad </td>
+              <td align="center"> Educación en<br>ingeniería </h6> </td>
+
+            </tr>
+       
+            <tr> 
+              <td align="center"> <br><img width="90" height="90" src="../imagenes/ambiente.png"> </td>
+              <td align="center"> <br><img width="90" height="90" src="../imagenes/gestion_empresarial.png"> </td>
+              <td align="center"> <br><img width="90" height="90" src="../imagenes/infraestructura.png"> </td>
+              <td align="center"><br><img width="90" height="90" src="../imagenes/logica.png"> </td>
+            </tr>
+            <tr> 
+              <td align="center">Energía<br>y ambiente  </td>
+              <td align="center"><div class="imagen"> Gestión empresarial<br>emprendimiento e<br>innovación </div></td>
+              <td align="center"> Infraestructura</td>
+              <td align="center"> Logística<br>y transporte</td>
+            </tr>
+
+            <tr> 
+              <td align="center"><br> <img width="90" height="90" src="../imagenes/robotica.png"> </td>
+              <td align="center"> <br><img width="90" height="90" src="../imagenes/comunicacion.png"> </td>
+              <td align="center"> <br><img width="90" height="90" src="../imagenes/tecnologia_emergente.png"> </td>
+            </tr>
+            <tr> 
+              <td align="center">Róbotica, automatización<br>e inteligencia artificial</td>
+              <td align="center"> Tecnologías de la información<br>y comunicación</td>
+              <td align="center"> Técnologías emergentes</td>
+            </tr>
+                
+            
+          </tr>   
+            
+             
+    </tbody>
+   
+   
+        
+        
+  
+  </table>
+
+</div>
+       
+     
+       
+       
+    </body>
+</html>
+
