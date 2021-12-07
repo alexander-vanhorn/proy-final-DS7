7ffb852056ae1b1c1782093aedaa5b0ca0c1118f
Noryadz <dayron6000@hotmail.com>
2021-12-07 01:53:52 -0500

    Añadidas imagenes/expositores/registro_asistencia
    Se añadieron las imagenes para expositores, la pantalla expositores y la pantalla registro_asistencia

---
+++ secciones/expositores.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="areas_de_interes.css">
+        <title>Expositores</title>
+    </head>
+    <body>
+        <div>
+            <br><br>
+            <h4 ><center><b>Expositores</b></center></h4>
+            <br><br>
+            <div class="table-responsive container">
+                <table  width="50%" border="0" align="center" >
+                    <tbody>
+                        <tr>
+                            <tr> 
+                                <td align="center"> <img  width="170" height="170" src="../imagenes/Forcael.png"></td>
+                                <td align="center"> <img width="170" height="170" src="../imagenes/RO-photo2.png"></td>
+                                <td align="center"> <img width="170" height="170" src="../imagenes/Al-Kodmany_10.png"></td>
+                            </tr>
+                            <tr>
+                                <td align="center"> DR. ERIC FORCAEL</td>
+                                <td align="center">DRA. ROSIBEL OCHOA</td>
+                                <td align="center">DR. CHRISTOPHER NUGENT</td>
+                            </tr>
+                            <tr> 
+                                <td align="center"> <br><img width="170" height="170" src="../imagenes/Manuel-quevedo-1.png"> </td>
+                                <td align="center"> <br><img width="170" height="170" src="../imagenes/granberg.png"> </td>
+                                <td align="center"> <br><img width="170" height="170" src="../imagenes/Alex-Alvarez2.png"> </td>
+                            </tr>
+                            <tr>
+                                <td align="center">DR. MANUEL QUEVEDO</td>
+                                <td align="center">DR. DOUGLAS D. GRANSBERG, PE</td>
+                                <td align="center">DR. ALEX ALVAREZ</td>
+                            </tr>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
+
