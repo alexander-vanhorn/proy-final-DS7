0bd003f7b900bf3aeb98a97c9b32706d9b078646
Jenia-Castillo <66661964+Jenia-Castillo@users.noreply.github.com>
2021-12-09 02:22:39 -0500

    giras tecnicas
    informacion de las giras tecnicas

---
+++ secciones/giras_tecnicas.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="areas_de_interes.css">
+        <title>Giras Técnicas</title>
+    </head>
+    <body>
+        <br>
+        <br>
+        <h4 ><center> <div class=titulo-texto>Giras Técnicas</div></center></h4>
+        <br>
+
+        <div class=texto >
+            <div class=subtitulo-texto><br>Giras disponibles  
+            </div>
+            
+           
+            <br> Las giras científicas - técnicas se llevará a cabo el sábado 12/10/2019.
+            <div class=subtitulo-texto>
+                <br>Esclusa de agua claras y nuevo puente sobre el canal colon:
+            </div>
+                
+<img class=img_aguasclaras src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpbs.twimg.com%2Fmedia%2FCi1rauJXIAABgz-.jpg&f=1&nofb=1">
+<img class=img_cocoli src="https://elcapitalfinanciero.com/wp-content/uploads/2016/06/esclusa-Pacifico-Cocoli-poblado-sector_LPRIMA20160330_0039_26-300x169.jpg">
+
+
+            08:30 am El bus recogerá a los pasajeros en el hotel de Wyndham más Panamá Albrook Mall
+            <br>10:00 am visita al centro de visitantes 
+            <br>12:00 pm paso sobre el puente Sherman 
+            <br>12:30 pm almuerzo en Sherman (No incluido)
+            <br>4:00 pm retomo al hotel Wyndahm Panamá Albrook Mall.
+            <br><b>Costo</b> USD 10.50 – Incluye la entrada, no la película.
+            Pagar en la mesa de registro.
+        
+
+           <div class=subtitulo-texto> 
+               <br>Esclusas de Miraflores:
+            </div>
+            <img class=img_miraflores src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.descubrir.com%2Fwp-content%2Fuploads%2F2020%2F01%2Fmiraflores.jpg&f=1&nofb=1">
+
+
+            08:30am—El bus recogerá a los pasajeros en el hotel Wyndham Panamá Albrook Mall 
+            <br>09:00am —Visita al centro de visitantes de Miraflores
+            <br>12:00am —Retorno al hotel Wyndham Panamá Albrook Mall
+            <br><b>Costo</b>  	USD10.50 incluye la entrada.
+
+            <div class=subtitulo-texto>
+                 <br>Esclusas de Cocolí:
+            </div>
+            6:30am- El bus recogerá a los pasajeros en el hotel Wyndham Panamá Albrook Mall 
+            <br>8:00am a 10:00am —Visita a las Esclusas de Cocolí
+            <br>10:30am — Retorno al hotel Wyndham Panamá Albrook Mall
+
+        </div >
+
+      
+
+
+         <!--
+
+         Las giras científicas - técnicas se llevará a cabo el sábado 12/10/2019.
+
+
+Esclusa de agua claras y nuevo puente sobre el canal colon:
+08:30 am El bus recogerá a los pasajeros en el hotel de Wyndham más Panamá Albrook Mall
+10:00 am visita al centro de visitantes 
+12:00 pm paso sobre el puente Sherman 
+1230 pm almuerzo en Sherman (No incluido)
+4:00 pm retomo al hotel Wyndahm Panamá Albrook Mall.
+Costo USD 10:50 – Incluye la entrada, no la película.
+Pagar en la mesa de registro.
+
+Esclusas de Miraflores:
+08:30am—El bus recogerá a los pasajeros en el hotel Wyndham Panamá Albrook Mall 
+09:00am —Visita al centro de visitantes de Miraflores
+12:00am —Retorno al hotel Wyndham Panamá Albrook Mall
+Costo 	USD10.50 incluye la entrada.
+Esclusas de Cocolí:
+6:30am- El bus recogerá a los pasajeros en el hotel Wyndham Panamá Albrook Mall 5:00am a 10:00am —Visita a las Esclusas de Cocolí
+10:30am — Retorno al hotel Wyndham Panamá Albrook Mall
+
+         -->
+
+     
+       
+       
+    </body>
+</html>
+
