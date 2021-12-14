8b39685b69400061879bf1ebbdd6b8feaea1cf68
DESKTOP-NVL0PV4\Alex <alexander.vanhorn@utp.ac.pa>
2021-12-14 00:48:37 -0500

    incoporación de css para página de inscripción y otros

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
     <!--CSS Externo para modificaciones-->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css\style.css">
     
     <!-- JavaScript Externo -->
-    <script src="style.js"></script>
+    <script src="js\style.js"></script>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -23,7 +23,7 @@
     <title>VIII Congreso IESTEC UTP</title>
 
     <!--Imagen de la barra de título-->
-    <link rel="shortcut icon" href="logo_utp_1_72.png" type="image/x-icon">
+    <link rel="shortcut icon" href="imagenes\logo_utp_color.png" type="image/x-icon">
 </head>
 
 <body>
@@ -34,7 +34,7 @@
     echo $x;
     ?>
 
-    <img class ="logo-utp" src="logo_utp_web.png">
+    <img class ="logo-utp" src="imagenes\logo_utp_web.png">
 
 
     <h1 class="titulo-1">VII Congreso Internacional de Ingeniería, Ciencias y Tecnología</h1>
@@ -43,7 +43,7 @@
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <footer>
     <br><br>
-    @2021 Universidad Tecnológica de Panamá || All rights reserved || Powered by UTP
+    @2021 Universidad Tecnológica de Panamá | All rights reserved | Powered by UTP
     <br><br><br>
 </footer>   
 
--- inscripcion.php
+++ inscripcion.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscripción</title>
+    <title>Congreso - Inscripción</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -12,7 +12,11 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+    <!--Imagen de la barra de título-->
+    <link rel="shortcut icon" href="imagenes\logo_utp_color.png" type="image/x-icon">
 
+    <!--CSS Externo para modificaciones-->
+    <link rel="stylesheet" href="css\estilos-inscripcion.css">
     
 </head>
 
@@ -23,13 +27,16 @@
     echo $x;
     ?>
 
+    
+
     <div class="encabezado">
-        <h3>Formulario de inscripción</h3>
+        <img class="logo-utp-color" src="imagenes\logo_utp_color.png">  
+        <br><br>
+        <h3>VIII Congreso IESTEC UTP 2022</h3>
+        <h5>Formulario de inscripción</h5>
         <small>Favor llena la información solicitada a continuación</small>
     </div>
     
-    
-
     <form class="row g-2">
         <div class="row g-2">
             <div class="col">
@@ -40,12 +47,12 @@
             </div>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-5">
             <label for="inputEmail4" class="form-label">Email</label>
             <input type="email" class="form-control" id="inputEmail4">
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-4">
             <label for="inputTipoDocmento" class="form-label">Tipo de Documento</label>
             <select class="form-select" id="specificSizeSelect">
             <option selected>Escoja una opción...</option>
@@ -55,8 +62,8 @@
         </div>
 
         <div class="col-md-3">
-            <label for="inputPassword4" class="form-label">Número de Documento</label>
-            <input type="password" class="form-control" id="inputPassword4">
+            <label for="inputPassword4" class="form-label">No. de Documento</label>
+            <input type="text" class="form-control" id="inputPassword4">
         </div>
 
         <div class="col-12">
@@ -65,7 +72,7 @@
         </div>
       
         <div class="col-12">
-            <label for="inputState" class="form-label">State</label>
+            <label for="inputState" class="form-label">Tipo de participante</label>
             <select id="inputState" class="form-select">
             <option selected>Escoja una opción</option>
             <option value="1">Estudinate Pregrado</option>
@@ -75,11 +82,13 @@
             <option value="5">Profesionales y otros Internacionales</option>
             </select>
         </div>
-     
+        
         <div class="col-12">
+            <br>
             <button type="submit" class="btn btn-primary">Registrarse</button>
         </div>
     </form>
+
     
 </body>
 </html>
