c9589674de8cba0f8bd2767a05aa2711af6016e7
Jenia-Castillo <66661964+Jenia-Castillo@users.noreply.github.com>
2021-12-13 23:41:15 -0500

    Certificado - correcciones
    olvidé tomar en cuenta la asistencia a la hora de generar el certificado.

--- secciones/certificado.php
+++ secciones/certificado.php
@@ -2,32 +2,39 @@
 
 
     //tutorial seguido https://youtu.be/M9oWxiocvjk
-    //include("../config/config.php");
+    
     include('../config/conexion.php');
 
     $id_ins=$_POST['idinscripcion'];
     require('fpdf.php');//biblioteca para convertir la imagen en un pdf 
-    //$sql="SELECT name from participantes where insc_id=$id_ins"; //esto lo hace por id cambiar a futuro por el que deberia buscar
-    //probando
-    
-    //$sql="SELECT part_id, nombre, apellido FROM participantes INNER JOIN inscripciones ON participantes.part_id=inscripciones.part_id";
-    
-    //$sql="SELECT participantes.nombre, participantes.apellido, inscripciones.insc_id FROM participantes INNER JOIN inscripciones ON participantes.part_id=inscripciones.part_id WHERE inscripciones.part_id=$id_ins"; 
-    $consultaCertificado =$conn->query("SELECT participantes.nombre, participantes.apellido, inscripciones.insc_id FROM participantes INNER JOIN inscripciones ON participantes.part_id=inscripciones.part_id WHERE inscripciones.part_id=$id_ins");
+    $asistencia=0;
+    //consulta es un inner join que consulta la tabla participantes e incripciones en donde el id_inscripcion sea igual al idinscripcion del metodo post
+    $consultaCertificado =$conn->query("SELECT participantes.nombre, participantes.part_id, participantes.apellido, inscripciones.insc_id FROM participantes INNER JOIN inscripciones ON participantes.part_id=inscripciones.part_id WHERE inscripciones.insc_id=$id_ins");
     //$consultaParticipantes = $conn->query("SELECT * FROM `participantes`");
     
-    //Se sacan los datos de la bd y se asignan a name
+    //Se sacan los datos de la bd y se asignan a name para colocarlo en la imagen del certificado
     while ($row = $consultaCertificado->fetch(PDO::FETCH_ASSOC)) {
         $name =$row['nombre']." ".$row['apellido'];
+        $parins_id=$row['part_id'];
+           
+    }
+    $consultaParticipantes_conf = $conn->query("SELECT * FROM participantes_conferencias WHERE part_id=$parins_id");
+
+     
+    while($row=$consultaParticipantes_conf  ->fetch(PDO::FETCH_OBJ)){ 
+     $asistencia++;
+       
+    
     }
 
+
     //$result=$conn->query($sql);
-    if(isset($name))
+   if((isset($name))&& ($asistencia>=3)) // si la variable existe y ha asistido la cantidad de veces necesaria
     {
         //$row=$result->fetch_assoc();//esto no entiendo bien que hace pero sirve
         
             $font="../certificado/arial.ttf";
-            $image = imagecreatefromjpeg("../imagenes/certificate.jpg");
+            $image = imagecreatefromjpeg("../imagenes/certificate.jpg");//certificado en blanco para que sea rellenado
             $color=imagecolorallocate($image,19,21,21);
             //$name=$row['nombre']." ".$row['apellido'];
             imagettftext($image, 35, 0, 400, 350, $color, $font, $name);//35 tamaño de letra, 0 angulo,400 y 350 posicion
@@ -51,7 +58,29 @@
             readfile('../certificado/certificado.pdf');
         
 
-    }else 
+    }elseif($asistencia<3){
+        
+        ?>
+
+            
+        <br>
+        <br>
+    
+
+        <center>
+        <div > <img class="img_error" width="10%" height="auto" src="../imagenes/nocumplir.png"><h1>Error</h1></div>
+        <h4>¡Vaya! Parece que no cumples con la asistencia necesaria para reclamar el certificado.
+        <br>
+        Cualquier reclamo por favor contactenos a correodeturno@gmail.com
+        </h4>
+        </center>
+
+
+    <?php
+
+    }
+    
+    else 
     { //pagina de error que se muestra si el usuario trató de meter una id de inscripcion que no existe
 
     
@@ -81,7 +110,7 @@
     if(extension_loaded('gd')&&function_exists('gd_info'))
     {
         //echo "GD installed";
-    } else{ echo"Hola parece que tienes gd desactivado asi que entra aqui https://youtu.be/IuQMKMtMLuU?t=129";
+    } else{ echo"Hola parece que tienes gd desactivado asi que entra aqui https://youtu.be/IuQMKMtMLuU?t=129 sigue los pasos y reinicia el apache";
     }
 
 
